c61f84a969071086e817d91aea360e1e6f330cd0
Justin Ball <justinball@gmail.com>
2009-05-14 15:37:19 -0600

    annotated model.  new grow config

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,35 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                  :integer(4)      not null, primary key
+#  login               :string(255)
+#  email               :string(255)
+#  first_name          :string(255)
+#  last_name           :string(255)
+#  crypted_password    :string(255)
+#  password_salt       :string(255)
+#  persistence_token   :string(255)
+#  single_access_token :string(255)
+#  perishable_token    :string(255)
+#  login_count         :integer(4)      default(0), not null
+#  failed_login_count  :integer(4)      default(0), not null
+#  last_request_at     :datetime
+#  last_login_at       :datetime
+#  current_login_at    :datetime
+#  current_login_ip    :string(255)
+#  last_login_ip       :string(255)
+#  terms_of_service    :boolean(1)      not null
+#  time_zone           :string(255)     default("UTC")
+#  disabled_at         :datetime
+#  activated_at        :datetime
+#  created_at          :datetime
+#  updated_at          :datetime
+#  photo_file_name     :string(255)
+#  photo_content_type  :string(255)
+#  photo_file_size     :integer(4)
+#
+
 class User < ActiveRecord::Base
   
   acts_as_authentic do |c|
