7b61126f51adb0333d0409e61a6da45b6701dab6
Justin Ball <justinball@gmail.com>
2009-05-27 23:11:28 -0600

    recommender plugin cleanup

--- vendor/plugins/recommender/db/migrate/20090416170254_add_stem_frequency.rb
+++
@@ -1,9 +0,0 @@
-class AddStemFrequency < ActiveRecord::Migration
-  def self.up
-  add_column :tags, :stem_frequency, :integer
-  end
-
-  def self.down
-   remove_column :tags, :stem_frequency
-  end
-end
--- vendor/plugins/recommender/db/migrate/20090430183806_add_language_to_feeds.rb
+++
@@ -1,9 +0,0 @@
-class AddLanguageToFeeds < ActiveRecord::Migration
-  def self.up
-  add_column :feeds, :language, :string
-  end
-
-  def self.down
-  remove_column :feeds, :language
-  end
-end
