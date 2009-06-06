9690d4dc6ddc4567d13e2ecc18c9883203750874
Joel Duffin <joel.duffin@gmail.com>
2009-06-06 17:19:16 -0600

    moved tag helper into the muck raker plugin

--- vendor/plugins/muck_raker/app/helpers/muck_raker_helper.rb
+++ vendor/plugins/muck_raker/app/helpers/muck_raker_helper.rb
@@ -1,11 +1,19 @@
-# Methods added to this helper will be available to all templates in the application.
-module ApplicationHelper
+module MuckRakerHelper
 
-  include TagsHelper
+  def tag_cloud(tags, classes)
+    max_count = tags.first.count.to_f
+    
+    atags = tags.sort_by(&:name)
+    atags.each do |tag|
+      index = ((tag.count / max_count) * (classes.size - 1)).round
+      yield tag,  classes[index]
+    end
+  end
   
   def round(flt)
     return (((flt.to_f*100).to_i.round).to_f)/100.0
   end
+  
   def truncate_words(text, length = 30, end_string = ' ...')
     words = text.split()
     words[0..(length-1)].join(' ') + (words.length > length ? end_string : '')
