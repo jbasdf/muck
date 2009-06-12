5c3edea72ca82744d03bab68d50b47818b209670
Justin Ball <justinball@gmail.com>
2009-06-11 19:01:41 -0600

    updated muck to use muck-engine gem

--- config/environment.rb
+++ config/environment.rb
@@ -27,7 +27,8 @@ Rails::Initializer.run do |config|
   config.gem 'cms-lite', :lib => 'cms_lite', :version => ">=0.3.3"
   config.gem 'disguise', :version => ">=0.2.0"
   config.gem 'uploader', :version => ">=0.1.15"
-  
+  config.gem 'muck-engine', :version => '>=0.1.0'
+
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
