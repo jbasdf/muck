7d29917b6cb674d4d8af796449b9216bda91c19e
Justin Ball <justinball@gmail.com>
2009-06-13 23:03:02 -0600

    updated gem version to match changes in activity engine

--- public/javascripts/muck_activities.js
+++ public/javascripts/muck_activities.js
@@ -1,6 +1,12 @@
 jQuery(document).ready(function() {
-	jQuery('.activity-no-comments').hide();
+	apply_comment_methods();
+});
+
+function apply_comment_methods(){
+	
 	hide_comment_boxes();
+	jQuery('.activity-no-comments').hide();
+	
 	jQuery('.activity-has-comments').find('textarea').click(function(){
 		show_comment_box(this);
 	});
@@ -10,7 +16,22 @@ jQuery(document).ready(function() {
 			hide_comment_boxes();
 		}
 	});
-});
+	
+	jQuery('.make-comment').click(function(){
+		var id = this.id.replace('make_comment_activity_', '');
+		var comment_box = jQuery('#comment_activity_' + id);
+		comment_box.find('textarea').removeClass('min');
+		comment_box.find('textarea').addClass('max');
+		comment_box.show();
+		comment_box.find('textarea').get(0).focus();
+		comment_box.find('textarea').blur(function(){
+			if (jQuery(this).val() == ''){
+				jQuery(this).closest('.activity-comment').hide();
+			}
+		});
+		return false;
+	});
+}
 
 function hide_comment_boxes(){
 	jQuery('.activity-has-comments').children('.actor-icon').hide();
@@ -23,7 +44,7 @@ function show_comment_box(obj){
 	textarea = jQuery(obj);
 	textarea.addClass('max');
 	textarea.removeClass('min');
-	textarea.siblings('.actor-icon').show();
+	textarea.closest('.comment-form-wrapper').siblings('.actor-icon').show();
 	textarea.siblings('.button').show();
 	if (textarea.val() == COMMENT_PROMPT) {
 		textarea.val('');
