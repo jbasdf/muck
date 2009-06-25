7a7459c382d9fdaf4eaed2761e73d250b5961c33
Joel Duffin <joel.duffin@gmail.com>
2009-06-25 17:29:09 -0600

    Fixed muck_raker initializer to correctly detect rails environment

--- config/initializers/muck_raker.rb
+++ config/initializers/muck_raker.rb
@@ -1,7 +1,7 @@
 SOLR_CONFIG_PATH = "#{RAILS_ROOT}/config/solr"
 SOLR_LOGS_PATH = "#{RAILS_ROOT}/log"
 SOLR_PIDS_PATH = "#{RAILS_ROOT}/tmp/pids"
-if RAILS_ENV == "production"
+if ENV['RAILS_ENV'] == "production"
   SOLR_DATA_PATH = "#{RAILS_ROOT}/../../shared/solr_indexes"
 else
   SOLR_DATA_PATH = "#{RAILS_ROOT}/../solr_indexes"
