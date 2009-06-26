73b0f48b0b74725d57daa63ebe74514914e1d996
Joel Duffin <joel.duffin@gmail.com>
2009-06-25 21:55:06 -0600

    Modified muck_raker initializer to set the rails environment based on a global config setting

--- config/initializers/muck_raker.rb
+++ config/initializers/muck_raker.rb
@@ -1,3 +1,4 @@
+ENV['RAILS_ENV'] = ENV['RAILS_ENV'] || GlobalConfig.rails_env  
 SOLR_CONFIG_PATH = "#{RAILS_ROOT}/config/solr"
 SOLR_LOGS_PATH = "#{RAILS_ROOT}/log"
 SOLR_PIDS_PATH = "#{RAILS_ROOT}/tmp/pids"
