6d1e455b868cc3248d77f3c1637fff053ca31e42
Justin Ball <justinball@gmail.com>
2009-06-29 13:42:03 -0600

    updates to match changes in gems

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,43 +13,44 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def set_locale
-    discover_locale
-  end
+    def set_locale
+      discover_locale
+    end
   
-  # only require ssl if we are in production
-  def ssl_required?
-    return false unless GlobalConfig.enable_ssl
-    return ENV['SSL'] == 'on' ? true : false if defined? ENV['SSL']
-    return false if local_request?
-    return false if RAILS_ENV == 'test'
-    ((self.class.read_inheritable_attribute(:ssl_required_actions) || []).include?(action_name.to_sym)) && (RAILS_ENV == 'production' || RAILS_ENV == 'staging')
-  end
+    # only require ssl if we are in production
+    def ssl_required?
+      return false unless GlobalConfig.enable_ssl
+      return ENV['SSL'] == 'on' ? true : false if defined? ENV['SSL']
+      return false if local_request?
+      return false if RAILS_ENV == 'test'
+      ((self.class.read_inheritable_attribute(:ssl_required_actions) || []).include?(action_name.to_sym)) && (RAILS_ENV == 'production' || RAILS_ENV == 'staging')
+    end
   
-  def setup_paging
-    @page = (params[:page] || 1).to_i
-    @page = 1 if @page < 1
-    @per_page = (params[:per_page] || (Rails.env=='test' ? 1 : 40)).to_i
-  end
+    def setup_paging
+      @page = (params[:page] || 1).to_i
+      @page = 1 if @page < 1
+      @per_page = (params[:per_page] || (Rails.env=='test' ? 1 : 40)).to_i
+    end
 
-  # Automatically respond with 404 for ActiveRecord::RecordNotFound
-  def record_not_found
-    render :file => File.join(RAILS_ROOT, 'public', '404.html'), :status => 404
-  end
+    # Automatically respond with 404 for ActiveRecord::RecordNotFound
+    def record_not_found
+      render :file => File.join(RAILS_ROOT, 'public', '404.html'), :status => 404
+    end
   
-  # called by Admin::Muck::BaseController to check whether or not the
-  # user should have access to the admin UI
-  def admin_access?
-    admin?
-  end
+    # called by Admin::Muck::BaseController to check whether or not the
+    # user should have access to the admin UI
+    def admin_access_required
+      access_denied unless admin?
+    end
 
   private
-  def choose_layout
-    if params[:controller].index('admin') == 0
-      'admin'
-    else
-      'default'
+
+    def choose_layout
+      if params[:controller].index('admin') == 0
+        'admin'
+      else
+        'default'
+      end
     end
-  end
 
 end
