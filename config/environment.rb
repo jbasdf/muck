912b6396f7da0f1c7cdbe497260e17e4cc36c8aa
Justin Ball <justinball@gmail.com>
2009-07-08 00:14:37 -0600

    removed friend items from profile page.  updated gem

--- config/environment.rb
+++ config/environment.rb
@@ -32,7 +32,7 @@ Rails::Initializer.run do |config|
   config.gem 'uploader', :version => '>=0.1.16'
   config.gem 'cms-lite', :lib => 'cms_lite', :version => '>=0.3.3'
   config.gem "muck-solr", :lib => 'acts_as_solr', :version => ">=0.4.1"
-  config.gem "muck-raker", :lib => 'muck_raker', :version => '>=0.1.11'
+  config.gem "muck-raker", :lib => 'muck_raker', :version => '>=0.1.12'
   config.gem 'muck-engine', :lib => 'muck_engine', :version => '>=0.1.16'
   config.gem 'muck-users', :lib => 'muck_users', :version => '>=0.2.0'
   config.gem 'muck-activities', :lib => 'muck_activities', :version => '>=0.1.13'
