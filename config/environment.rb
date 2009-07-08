53c964f31e7f74efcd4c15bb55a2637f6c1cb1dd
Justin Ball <justinball@gmail.com>
2009-07-08 00:23:06 -0600

    updated gem version

--- config/environment.rb
+++ config/environment.rb
@@ -37,7 +37,7 @@ Rails::Initializer.run do |config|
   config.gem 'muck-users', :lib => 'muck_users', :version => '>=0.2.0'
   config.gem 'muck-activities', :lib => 'muck_activities', :version => '>=0.1.13'
   config.gem 'muck-comments', :lib => 'muck_comments', :version => '>=0.1.5'
-  config.gem 'muck-profiles', :lib => 'muck_profiles', :version => '>=0.1.3'
+  config.gem 'muck-profiles', :lib => 'muck_profiles', :version => '>=0.1.4'
   config.gem 'muck-friends', :lib => 'muck_friends', :version => '>=0.1.1'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
