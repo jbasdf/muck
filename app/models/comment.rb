870363bf5c61f17fd6e462c3e26e327e53e52ac5
Justin Ball <justinball@gmail.com>
2009-08-08 22:01:22 -0600

    commented out activity create in comment for now

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,14 +2,14 @@ class Comment < ActiveRecord::Base
   acts_as_muck_comment
   acts_as_activity_source
   
-  def after_create
-    # Only add activity entries for specific kinds of comments.  Otherwise the comments could show up multiple times.  
-    # Comments are attached to items in the feed.  If you add an activity entry for each new comment it will show 
-    # up as a top level item in the feed as well as under the item that is in the feed.
-    if self.commentable.is_a?(Entry) # An entry doesn't automatically show up in the activity feed so 
-      debugger
-      add_activity(self.user.feed_to, self.user, self, 'entry', '', '')
-    end
-  end
+  # def after_create
+  #   # Only add activity entries for specific kinds of comments.  Otherwise the comments could show up multiple times.  
+  #   # Comments are attached to items in the feed.  If you add an activity entry for each new comment it will show 
+  #   # up as a top level item in the feed as well as under the item that is in the feed.
+  #   if self.commentable.is_a?(Entry) # An entry doesn't automatically show up in the activity feed so 
+  #     debugger
+  #     add_activity(self.user.feed_to, self.user, self, 'entry', '', '')
+  #   end
+  # end
   
 end
