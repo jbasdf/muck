25bf32016722ece22f9706c66da5c68f40538d25
Justin Ball <justinball@gmail.com>
2009-09-14 13:31:33 -0600

    updated muck raker version

--- config/environment.rb
+++ config/environment.rb
@@ -38,7 +38,7 @@ Rails::Initializer.run do |config|
   config.gem 'cms-lite', :lib => 'cms_lite', :version => '>=0.5.3'
   config.gem 'muck-engine', :lib => 'muck_engine', :version => '>=0.2.0'
   config.gem "muck-solr", :lib => 'acts_as_solr', :version => '>=0.4.2'
-  config.gem "muck-raker", :lib => 'muck_raker', :version => '>=0.1.28'
+  config.gem "muck-raker", :lib => 'muck_raker', :version => '>=0.1.29'
   config.gem 'muck-users', :lib => 'muck_users', :version => '>=0.2.11'
   config.gem 'muck-activities', :lib => 'muck_activities', :version => '>=0.1.20'
   config.gem 'muck-comments', :lib => 'muck_comments', :version => '>=0.1.10'
