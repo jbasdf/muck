ee7f8ea0e0f6f1fdd31df6d613f0eccc040439f5
Justin Ball <justinball@gmail.com>
2009-10-01 01:39:06 -0600

    updated versions

--- config/environment.rb
+++ config/environment.rb
@@ -38,7 +38,7 @@ Rails::Initializer.run do |config|
   config.gem 'cms-lite', :lib => 'cms_lite', :version => '>=0.5.4'
   config.gem 'muck-engine', :lib => 'muck_engine', :version => '>=0.2.1'
   config.gem "muck-solr", :lib => 'acts_as_solr', :version => '>=0.4.3'
-  config.gem "muck-raker", :lib => 'muck_raker', :version => '>=0.1.35'
+  config.gem "muck-raker", :lib => 'muck_raker', :version => '>=0.1.36'
   config.gem 'muck-users', :lib => 'muck_users', :version => '>=0.2.12'
   config.gem 'muck-activities', :lib => 'muck_activities', :version => '>=0.1.21'
   config.gem 'muck-comments', :lib => 'muck_comments', :version => '>=0.1.11'
