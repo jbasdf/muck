b20d530f7509cfb088b87915cde85b22a205d8d7
Justin Ball <justinball@gmail.com>
2009-10-20 15:51:49 -0600

    synced in muck raker.  Removed cms lite

--- config/initializers/cms_lite.rb
+++ config/initializers/cms_lite.rb
@@ -1,2 +1,2 @@
-CmsLite.cms_layouts = { :default => 'default' }
-CmsLite.append_content_path("themes/#{Theme.first.name}/content") if Theme.first && File.exists?("#{RAILS_ROOT}/themes/#{Theme.first.name}/content")
+# CmsLite.cms_layouts = { :default => 'default' }
+# CmsLite.append_content_path("themes/#{Theme.first.name}/content") if Theme.first && File.exists?("#{RAILS_ROOT}/themes/#{Theme.first.name}/content")
